a393930eb609a09e79302bb107b0c2efd0d8642c
Tomer Eiges <tomer1510@gmail.com>
2015-07-10 18:08:44 +0300

    added more styling

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,20 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+/**
+ * JS Library v0
+ */
 
-    while (length--) {
-        method = methods[length];
+var __ = (function () {
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+    return {
+        /**
+         * Check if a given value is a plain Object
+         *
+         * @param  {*}       o Any value to be checked
+         * @return {Boolean}   true if it's an Object
+         */
+        isObject: function (o) {
+            var toString = Object.prototype.toString;
+            return (toString.call(o) === toString.call({}));
         }
-    }
+    
+    };
 }());
-
-// Place any jQuery/helper plugins in here.
